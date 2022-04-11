7e385ace94ca91a3b0107ede16ea17256d7d100c
RossenLin <jetso1127@gmail.com>
2022-04-11 14:55:21 +0800

    del

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,6 @@ module.exports = {
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-shopping-cart/' // test20200915 為 repo 名稱
+    ? '/vue-shopping-cart/' // 此為 repo 名稱
     : '/'
 }
